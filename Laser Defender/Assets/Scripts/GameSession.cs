2a2f602b68960e062d091ded0bc9cf0cf11c45e8
FD <firiondarklight@msn.com>
2018-11-26 21:40:27 +0200

    first upload ever
    yaiii

---
+++ Laser Defender/Assets/Scripts/GameSession.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour {
+
+    int score = 0;
+    private static GameSession  _instance;
+
+    public GameSession Gamesession {
+        get { return _instance; }
+        set { _instance = value; }
+    }
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        if(_instance!= null && _instance!=this) {
+            Destroy(this);
+        }
+        else {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddToScore(int scoreValue)
+    {
+        score += scoreValue;
+    }
+
+    public void ResetGame()
+    {
+        //Destroy(gameObject);
+    }
+
+ }
